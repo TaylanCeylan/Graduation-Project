639558c3ab8cc5d73675fab7a423908702fce9cb
RetrioNN <69519588+RetrioNN@users.noreply.github.com>
2022-06-01 22:42:40 +0300

    .

--- Action Platformer/Assets/Scripts/Menu/MainMenu.cs
+++ Action Platformer/Assets/Scripts/Menu/MainMenu.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject controls;
+
+    private void Update()
+    {
+        ActivateControls();
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -15,4 +22,18 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
     
+    public void ActivateControls()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            controls.SetActive(false);
+        }
+    }
+
+    public void OpenControls()
+    {
+        controls.SetActive(true);
+    }
+
 }
